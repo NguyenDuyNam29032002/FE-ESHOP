2daf7e030d61cb30c896914d424edffe5a1d49de
Admin <NguyenDuyNam29032002>
2022-12-31 20:14:43 +0700

    display banner

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * @method static latest()
@@ -12,4 +13,5 @@ class Banner extends Model
 {
     protected $table = 'banners';
     use HasFactory;
+    use SoftDeletes;
 }
